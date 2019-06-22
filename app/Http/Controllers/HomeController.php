ccc2c5db7f4d5e28a63c57df63b55264e1de7e63
My name is who <mynameis_who@example.com>
2019-06-22 04:58:38 +0000

    add test code in HomeController

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+//use Log;
+//test hogeeeeeeeeeeeee
+
+//use App\Http\Services\CalculationService;
+use App\Http\Services\CalculationService;
+use App\User;
 
 class HomeController extends Controller
 {
@@ -11,11 +17,51 @@ class HomeController extends Controller
      *
      * @return void
      */
-    public function __construct()
+    public function __construct(User $user, User $user2, CalculationService $service)
     {
-        $this->middleware('auth');
+        $this->user = $user;
+        $this->user2 = $user2;
+//echo ">>>aaa<br>\n";
+//print_r($user);
+//echo ">>>bbb<br>\n";
+//print_r($user2);
+
+        // タイプヒントだけで使える気がする
+        // 検証元ネタ：https://www.ritolab.com/entry/108
+echo ">>>ccc<br>\n";
+print_r($service->add(1,1));
+$service2 = app()->make(CalculationService::class);
+echo ">>>ccc222<br>\n";
+print_r($service->add(1,11));
+
+
+
+//        $this->middleware('auth');
     }
 
+    public function search(Request $request)
+    {
+//        Log::debug(">>".$request);
+//        \Log::debug(">>".print_r($request->all()));
+//        \Log::debug('here');
+
+//        $result = [
+//            'status' => '500',
+//            'message' => 'error',
+//        ];
+//        return ['result' => 1];
+//        return ['apple' => 'red', 'peach' => 'pink'];
+//        return response()->json(['apple' => 'red', 'peach' => 'pink']);
+//        return response()->json(['apple' => 'red', 'peach' => 'pink']);
+        $result = array(
+            'status' => '500',
+            'message' => '使用済み',
+        );
+//        return view('home');
+//        return response()->json($result);
+        return ['result' => $result];
+//        return 1;
+    }
     /**
      * Show the application dashboard.
      *
@@ -40,9 +86,16 @@ class HomeController extends Controller
     {
         return view('home_top_3');
     }
-    public function mock_4()
+//    public function mock_4(User $user, User $user2)
+    public function mock_4(User $user)
     {
-        return view('home_top_4');
+        $user2 = app()->make(User::class);
+print_r($user);
+echo ">>>222<br>\n";
+print_r($user2);
+//        print_r($user3);
+//        return view('home_top_4');
+        return view('home_top_4_2');
     }
     public function mock_5()
     {
